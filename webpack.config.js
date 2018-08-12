2d824b76658f2a4bbed3c7c29b7729bf44120dfe
Julia <juliabower@outlook.co.nz>
2018-08-12 16:30:17 +1200

    move babel config into own file

--- webpack.config.js
+++ webpack.config.js
@@ -13,10 +13,6 @@ module.exports = {
         exclude: /node_modules/,
         use: {
           loader: "babel-loader",
-          options: {
-            presets: ["env", "react"],
-            plugins: ["transform-class-properties"],
-          }
         }
       },
       {
