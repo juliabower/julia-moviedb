01de7ef3b02c458584a637799cc0aad049e934bc
Julia <juliabower@outlook.co.nz>
2018-08-12 13:15:19 +1200

    call api from seach bar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,18 +1,39 @@
-import React from "react";
+import React, { Component } from "react";
+import { searchMovie } from "../api/movieDB-api";
+class SearchBar extends Component {
+  componentDidMount() {
+    this.state = {
+      search: '',
+    };
+  }
 
-const SearchBar = () => {
-  return (
-    <div className="row">
-        <div class="col-lg-12">
-          <div class="input-group">
-            <input type="text" className="form-control" placeholder="e.g. Harry Potter" />
+  updateSearch = (e) => {
+    this.setState({ search: e.target.value });
+  }
+
+  search = () => {
+    this.setState({ error: false });
+    searchMovie(this.state.search).then(response => {
+      debugger;
+      this.setState({ results: response.results });
+    }).catch(error => this.setState({ error: true })
+    );
+  }
+
+  render() {
+    return (
+      <div className="row" >
+        <div className="col-lg-12">
+          <div className="input-group">
+            <input type="text" className="form-control" placeholder="e.g. Harry Potter" onChange={this.updateSearch} />
             <span className="input-group-btn">
-              <button className="btn btn-default btn-go" type="button">Go!</button>
+              <button className="btn btn-default btn-go" type="button" onClick={this.search}>Go</button>
             </span>
           </div>
         </div>
       </div>
-  );
+    );
+  }
 }
 
 export default SearchBar;
