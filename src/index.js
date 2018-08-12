3e0a7adde1f09696a7a4fc311fbf92e835e1a0b3
Julia <juliabower@outlook.co.nz>
2018-08-12 12:34:40 +1200

    add style scss file, add search bar

--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
 
 import React from "react";
 import ReactDOM from "react-dom";
+import "./styles.scss";
 
 const Index = () => {
-  return <div>Search for a movie</div>;
+  return (
+    <main className="container">
+      <h1>Search for a movie</h1>
+      <div className="row">
+        <div class="col-lg-12">
+          <div class="input-group">
+            <input type="text" className="form-control" placeholder="Search for..." />
+            <span className="input-group-btn">
+              <button className="btn btn-default btn-go" type="button">Go!</button>
+            </span>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
 };
-
 ReactDOM.render(<Index />, document.getElementById("index"));
