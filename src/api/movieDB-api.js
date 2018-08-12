25219e17be5c302ed2806b620646bfd20468ce4a
Julia <juliabower@outlook.co.nz>
2018-08-12 13:15:06 +1200

    add api file

---
+++ src/api/movieDB-api.js
@@ -0,0 +1,12 @@
+const apiKey = "a8f1ca842faf07c1f6f4f875b9c810a9";
+const baseUrl = "https://api.themoviedb.org";
+
+const getUrl = (query, page) => {
+  return `${baseUrl}/3/search/movie?api_key=${apiKey}&query=${query}&page=${page}`;
+}
+
+export const searchMovie = (query, page = 1) => {
+  return fetch(getUrl(query, page))
+    .then(response => response.json())
+    .catch(err);
+}
