ca577122280be5b5b01329c62317bc8ea087eb0f
Julia <juliabower@outlook.co.nz>
2018-08-12 12:33:43 +1200

    add file, style, css and sass loaders to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,36 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
+
 const htmlPlugin = new HtmlWebPackPlugin({
   template: "./src/index.html",
   filename: "./index.html"
 });
 
 module.exports = {
-    module: {
-      rules: [
-        {
-          test: /\.js$/,
-          exclude: /node_modules/,
-          use: {
-            loader: "babel-loader",
-            options: {
-              presets: ['env', 'react'],
-            }
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["env", "react"],
           }
         }
-      ]
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          "style-loader",
+          "css-loader",
+          "sass-loader"
+        ]
+      },
+      {
+        test: /\.woff2?$|\.ttf$|\.eot$|\.svg$/,
+        loader: 'file-loader'
     },
-    plugins: [htmlPlugin]
-  };
+    ]
+  },
+  plugins: [htmlPlugin]
+};
